7529f31a27d25c5e67c39f34e030a65b1a08d6f9
Christian Thrige Nielsen <149200558+CN-Thrige@users.noreply.github.com>
2026-03-09 20:29:10 +0100

    Add files via upload

---
+++ Circle calculator/CircleCal_V1.py
@@ -0,0 +1,20 @@
+
+r = int(input("Hvad er radius, skriv et tal her"))
+#kun heltal og skriv et tal i selve program.
+
+pi= 3.14
+#definerer pi
+
+print (pi*r**2)
+
+print(("Radius:"), r, ("cm"))
+#udregning af Radius
+
+print(("Areal:"),pi*r**2, ("cm"))
+#udregning af areal
+
+print(("Omkreds:"),2*pi*r, ("cm"))
+#udregning af omkreds
+
+
+
